94c35f1f885a13248ca38d970691b0a2443ac442
JorgeNMoctezuma <123213442+JorgeNMoctezuma@users.noreply.github.com>
2023-01-28 21:45:54 -0600

    Create Main.java

---
+++ src/edu/Jorge/Nicolas/Moctezuma/Arellano/reto1/Main.java
@@ -0,0 +1,10 @@
+package edu.Jorge.Nicolas.Moctezuma.Arellano.reto1;
+
+import edu.Jorge.Nicolas.Moctezuma.Arellano.reto1.ui.CLI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CLI.mostrarMenu();
+    }
+}
